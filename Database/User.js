6f601a5f80cad0c98869dc75ca687c56185e88e0
(˃ᆺ˂) <2300031042@kluniversity.in>
2024-10-29 23:18:08 +0530

    third commit

--- Database/User.js
+++ Database/User.js
@@ -2,25 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-let cachedConnection = null;
-
 const connectDB = async () => {
-  if (cachedConnection) {
-    return cachedConnection;
-  }
-
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000,
-      maxPoolSize: 10,
-    });
-    
-    cachedConnection = conn;
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("Database Connected");
-    return conn;
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
-    throw err;
   }
 };
 
--- index.js
+++ index.js
@@ -9,10 +9,11 @@ import courseRouter from "./routes/course.routes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 connectDB()
   .then(() => {
@@ -23,14 +24,6 @@ connectDB()
   .catch((err) => {
     console.error("Failed to connect to database:", err);
   });
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ 
-    message: 'Something went wrong!',
-    error: process.env.NODE_ENV === 'development' ? err.message : 'Internal server error'
-  });
-});
 
 app.listen(process.env.PORT || 3009, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
